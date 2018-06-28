f59fed20e20e25566c4270c39546254bb2b2850f
Matthew Goldfine <mgoldfi1@binghamton.edu>
2018-06-28 15:13:34 +0000

    automatically backed up by learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -1,9 +1,11 @@
 class Transfer
-attr_reader :transfer
+attr_reader :amount, :sender, :receiver
 attr_accessor :status
 
-def initialize
-
+def initialize(sender,receiver,amount)
+  @sender = sender
+  @receiver = receiver
+  @amount = amount
   @status = "pending"
 end
 
