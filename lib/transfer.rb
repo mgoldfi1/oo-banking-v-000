40547f8bd78c3fe11692dd76b46630d8884e956c
Matthew Goldfine <mgoldfi1@binghamton.edu>
2018-06-28 15:58:24 +0000

    automatically backed up by learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -18,20 +18,18 @@ def execute_transaction
   if self.valid? == false || self.amount > sender.balance
       self.status = "rejected"
       "Transaction rejected. Please check your account balance."
-  elsif self.status == "complete"
-    self.amount = 0
+
   elsif self.status == "pending"
   sender.balance -= self.amount
   receiver.balance += self.amount
   self.status = "complete"
-
-
-
-
-
+  end
 end
 
-end
+
+def reverse_transfer
+  if self.status = "complete"
+  end
 
 
 
